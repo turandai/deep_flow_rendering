3395d2bcfac0e8cfa8db94fbb8f0727b01f7aebf
Turan <25923503+turandai@users.noreply.github.com>
2023-03-21 20:54:19 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -9,7 +9,7 @@ import time
 import sys
 import random
 import argparse
-# personal
+
 from dataIO import *
 from transform import *
 from division import *
@@ -127,19 +127,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--scene', type=str, default=model)
 args = parser.parse_args()
 
-if model == 'spot.obj':
-    obj_data['m2w'] = translate(0, 0, 0)
-    camera['origin'] = [0, 0, -3.9]
-    ref_path = data_path/'spot_box_360/'
-    tex_file = data_path/'spot_box_360/texture.png'
-    ref_seq = [[x, y] for x in range(0, 1, 2) for y in range(0, 361, 10)]    
-    data_arrange = [
-        [1, 10  ],  # test_gap
-        [1, 10  ],  # train_gap
-        [0, 0  ],  # x_range
-        [0, 360],  # y_range
-    ]
-elif model == 'ball':
+if model == 'ball':
     data_type = 'synth'
     data_path = data_path/'ball'
     mesh_path = data_path/'ball.obj'
@@ -150,99 +138,16 @@ elif model == 'ball':
     camera['height'] = 512
     camera['width'] = 512
     ow_reso=None
-    img_path = data_path/'al/'
+    img_path = data_path/'images'
     train_seq = [i for i in range(0, 181, 10)]
     test_seq = [i for i in range(0, 181, 1) if i % 10 != 0]
     render_seq = [i for i in range(0, 181, 1)]
     views = [[x, y] for x in range(0, 1, 1) for y in range(0, 361, 2)]
-elif model == 'birdvase':
-    data_type = 'synth'
-    data_path = data_path/'birdvase'
-    mesh_path = data_path/'vase.obj'
-    m2w = np.matmul(translate(0, -1.1, 0), np.matmul(rotate_x(np.pi / 2), scale(0.0035)))
-    camera = read_camera_config(f'{data_path}/../{camera_file}')
-    camera['fov'] = 39
-    camera['origin'] = [0, 0, -3.9]
-    camera['height'] = 1024
-    camera['width'] = 512
-    ow_reso=None
-    img_path = data_path/'indoor/'
-    # tex_file = data_path/'vase_box/texture360.png'
-    train_seq = [i for i in range(0, 361, 5)]
-    test_seq = [i for i in range(0, 361, 1) if i % 5 != 0]
-    render_seq = [i for i in range(0, 361, 1)]
-    views = [[x, y] for x in range(0, 1, 1) for y in range(0, 361, 1)]
-elif model == 'plate.obj':
-    obj_data['m2w'] = np.matmul(translate(0, 0, 0), np.matmul(rotate_x(np.pi / 2), scale(0.01)))
-    camera['origin'] = [0, 0, -3.9]
-    camera['height'] = camera['width'] = 1024
-    ref_path = data_path/'plate/ref/'
-    tex_file = data_path/'plate/texture.png'
-    # ref_seq = [[x, y] for x in range(-60, -59, 2) for y in range(0, 361, 2)]
-    ref_seq = [[x, y] for x in range(-80, -60 + 1, 2) for y in range(30, 60 + 1, 2)]
-    data_arrange = [
-        [2, 2],   # test_gap
-        [4, 4],   # train_gap
-        [-80, -60],   # x_range
-        [30, 60], # y_range
-    ]
-elif model == 'teacup_4e':
+elif model == 'brownpot':
     data_type = 'real'
-    data_path = data_path/'teacup_4e'
+    data_path = data_path/'brownpot'
     mesh_path = data_path/'mesh.obj'
-    ref_path = data_path/'img'
-    imglist_path = data_path/'images.txt'
-    cmrlist_path = data_path/'cameras.txt'
-    m2w = translate(0, 0, 0)
-    ref_seq = [i for i in range(0, 261, 5)]
-    img_path = [f'{ref_path}/img_{i}.jpg' for i in ref_seq] 
-    train_seq = [i for i in range(0, 53, 4)]
-    test_seq = [i for i in range(0, 53, 2) if i % 4 != 0]
-    render_seq = [i for i in range(0, 53, 1)]
-elif model == 'tea_cup_6e':
-    data_type = 'real'
-    data_path = data_path/'tea_cup_6e'
-    mesh_path = data_path/'mesh.obj'
-    ref_path = data_path/'img'
-    imglist_path = data_path/'images.txt'
-    cmrlist_path = data_path/'cameras.txt'
-    m2w = translate(0, 0, 0)
-    ref_seq = [i for i in range(0, 261, 5)]
-    img_path = [f'{ref_path}/img_{i}.jpg' for i in ref_seq] 
-    train_seq = [i for i in range(0, 53, 6)]
-    test_seq = [i for i in range(0, 53, 2) if i % 6 != 0]
-    render_seq = [i for i in range(0, 53, 1)]
-elif model == 'xipot_4e':
-    data_type = 'real'
-    data_path = data_path/'xipot_4e'
-    mesh_path = data_path/'mesh.obj'
-    ref_path = data_path/'img'
-    imglist_path = data_path/'images.txt'
-    cmrlist_path = data_path/'cameras.txt'
-    m2w = translate(0, 0, 0)
-    ref_seq = [i for i in range(0, 406, 5)]
-    img_path = [f'{ref_path}/img_{i}.jpg' for i in ref_seq] 
-    train_seq = [i for i in range(0, 81, 4)]
-    test_seq = [i for i in range(0, 81, 2) if i % 4 != 0]
-    render_seq = [i for i in range(0, 81, 1)]
-elif model == 'xi_pot_6e':
-    data_type = 'real'
-    data_path = data_path/'xi_pot_6e'
-    mesh_path = data_path/'mesh.obj'
-    ref_path = data_path/'img'
-    imglist_path = data_path/'images.txt'
-    cmrlist_path = data_path/'cameras.txt'
-    m2w = translate(0, 0, 0)
-    ref_seq = [i for i in range(0, 406, 5)]
-    img_path = [f'{ref_path}/img_{i}.jpg' for i in ref_seq] 
-    train_seq = [i for i in range(0, 81, 6)]
-    test_seq = [i for i in range(0, 81, 2) if i % 6 != 0]
-    render_seq = [i for i in range(0, 81, 1)]
-elif model == 'brwpot_4e':
-    data_type = 'real'
-    data_path = data_path/'brwpot_4e'
-    mesh_path = data_path/'mesh.obj'
-    ref_path = data_path/'img'
+    ref_path = data_path/'images'
     imglist_path = data_path/'images.txt'
     cmrlist_path = data_path/'cameras.txt'
     m2w = translate(0, 0, 0)
@@ -252,59 +157,6 @@ elif model == 'brwpot_4e':
     train_seq = [i for i in range(0, 75, 4)]
     test_seq = [i for i in range(0, 75, 2) if i % 4 != 0]
     render_seq = [i for i in range(0, 75, 1)]
-elif model == 'mao_badge':
-    data_type = 'real'
-    data_path = data_path/'mao_badge'
-    mesh_path = data_path/'mesh.obj'
-    ref_path = data_path/'img'
-    imglist_path = data_path/'images.txt'
-    cmrlist_path = data_path/'cameras.txt'
-    m2w = translate(0, 0, 0)
-    ref_seq = [i for i in range(0, 806, 5)]
-    img_path = [f'{ref_path}/img_{i}.jpg' for i in ref_seq] 
-    train_seq = [i for i in range(0, 162, 6)]
-    test_seq = [i for i in range(0, 162, 2) if i % 6 != 0]
-    render_seq = [i for i in range(0, 162, 1)]
-elif model == 'violin_6e':
-    data_type = 'real'
-    data_path = data_path/'violin_6e'
-    mesh_path = data_path/'mesh.obj'
-    ref_path = data_path/'img'
-    imglist_path = data_path/'images.txt'
-    cmrlist_path = data_path/'cameras.txt'
-    m2w = translate(0, 0, 0)
-    ref_seq = [i for i in range(0, 626, 5)]
-    img_path = [f'{ref_path}/img_{i}.jpg' for i in ref_seq] 
-    train_seq = [i for i in range(0, 126, 6)]
-    test_seq = [i for i in range(0, 126, 2) if i % 6 != 0]
-    render_seq = [i for i in range(0, 126, 1)]
-elif model == 'violin_12e':
-    data_type = 'real'
-    data_path = data_path/'violin_12e'
-    mesh_path = data_path/'meshD.obj'
-    ref_path = data_path/'img'
-    imglist_path = data_path/'images.txt'
-    cmrlist_path = data_path/'cameras.txt'
-    m2w = translate(0, 0, 0)
-    ref_seq = [i for i in range(0, 626, 5)]
-    img_path = [f'{ref_path}/img_{i}.jpg' for i in ref_seq] 
-    train_seq = [i for i in range(0, 126, 12)]
-    test_seq = [i for i in range(0, 126, 2) if i % 12 != 0]
-    render_seq = [i for i in range(0, 126, 1)]
-elif model == 'book_6e':
-    data_type = 'real'
-    data_path = data_path/'book_6e'
-    mesh_path = data_path/'meshD.obj'
-    ref_path = data_path/'img'
-    imglist_path = data_path/'images.txt'
-    cmrlist_path = data_path/'cameras.txt'
-    m2w = translate(0, 0, 0)
-    ref_seq = [i for i in range(0, 411, 5)]
-    img_path = [f'{ref_path}/img_{i}.jpg' for i in ref_seq] 
-    train_seq = [i for i in range(0, 83, 6)]
-    test_seq = [i for i in range(0, 83, 2) if i % 6 != 0]
-    render_seq = [i for i in range(0, 83, 1)]
-# final 
 elif model == 'teacup':
     data_type = 'real'
     data_path = data_path/'teacup'
@@ -336,48 +188,6 @@ elif model == 'xipot':
     train_seq = [i for i in range(0, 81, 4)]
     test_seq = [i for i in range(0, 81, 1) if i % 4 != 0]
     render_seq = [i for i in range(0, 81, 1)]
-elif model == 'bluepot':
-    data_type = 'real'
-    data_path = data_path/'bluepot'
-    mesh_path = data_path/'meshD.obj'
-    ref_path = data_path/'images_640'
-    imglist_path = data_path/'images.txt'
-    cmrlist_path = data_path/'cameras.txt'
-    m2w = translate(0, 0, 0)
-    ow_reso = [640, 640]
-    ref_seq = [i for i in range(0, 281, 5)]
-    img_path = [f'{ref_path}/img_{i}.jpg' for i in ref_seq] 
-    train_seq = [i for i in range(0, 57, 4)]
-    test_seq = [i for i in range(0, 57, 1) if i % 4 != 0]
-    render_seq = [i for i in range(0, 57, 1)]
-elif model == 'sofa':
-    data_type = 'real'
-    data_path = data_path/'sofa'
-    mesh_path = data_path/'mesh.obj'
-    ref_path = data_path/'images'
-    imglist_path = data_path/'images.txt'
-    cmrlist_path = data_path/'cameras.txt'
-    m2w = translate(0, 0, 0)
-    ow_reso = None
-    ref_seq = [i for i in range(0, 641, 5)]
-    img_path = [f'{ref_path}/img_{i}.jpg' for i in ref_seq] 
-    train_seq = [i for i in range(0, 129, 4)]
-    test_seq = [i for i in range(0, 129, 1) if i % 4 != 0]
-    render_seq = [i for i in range(0, 129, 1)]
-elif model == 'photo':
-    data_type = 'real'
-    data_path = data_path/'photo'
-    mesh_path = data_path/'mesh.obj'
-    ref_path = data_path/'images'
-    imglist_path = data_path/'images.txt'
-    cmrlist_path = data_path/'cameras.txt'
-    m2w = translate(0, 0, 0)
-    ow_reso = None
-    ref_seq = [i for i in range(0, 221, 5)]
-    img_path = [f'{ref_path}/img_{i}.jpg' for i in ref_seq] 
-    train_seq = [i for i in range(0, 45, 4)]
-    test_seq = [i for i in range(0, 45, 1) if i % 4 != 0]
-    render_seq = [i for i in range(0, 45, 1)]
 elif model == 'bluevase':
     data_type = 'real'
     data_path = data_path/'bluevase'
@@ -389,10 +199,10 @@ elif model == 'bluevase':
     ow_reso = [640, 640]
     ref_seq = [i for i in range(0, 571, 5)]
     img_path = [f'{ref_path}/img_{i}.jpg' for i in ref_seq] 
-    # missing #21, #22
     train_seq = [i for i in range(0, 115, 6)]
     test_seq = [i for i in range(0, 115, 1) if i % 6 != 0]
     render_seq = [i for i in range(0, 115, 1)]
+    # colmap missing #21, #22, #31
     for i in range(len(train_seq)):
         if train_seq[i] >= 21:
             train_seq[i] -= 1
